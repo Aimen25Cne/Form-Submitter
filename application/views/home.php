42072cd1513de85bedff0d3558129321c993dea1
Aimen25Cne <aimen25cne@gmail.com>
2017-12-10 13:50:46 +0100

    delete yt link

--- application/views/home.php
+++ application/views/home.php
@@ -1,5 +1,4 @@
 <div class="container">
-  <iframe width="560" height="315" src="https://www.youtube.com/embed/lzEfuhcjpW4" frameborder="0" allowfullscreen></iframe>
 </div>
 <br>
 <div class="container">
